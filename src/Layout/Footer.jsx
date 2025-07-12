c1f88b19f6bec88cf0f3c5dace53b496fd9f3799
Emiri Sato <emirisato.1003@gmail.com>
2025-07-11 19:27:27 -0700

    styling home page.

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styles from './Footer.module.css'
+
 //react icon
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
@@ -6,20 +8,24 @@ import { FaYoutube } from "react-icons/fa";
 export default function Footer() {
     return (
         <footer>
-            <div className="footer-top">
-                <div className="footer-contents">
-                    <h2>Emiri Sato</h2>
-                    <p>A front-end developer crafting seamless, user-friendly websites and web applications from concept to completion with a focus on great design and usability.</p>
+            <div className={styles.footerContainer}>
+                <div className={styles.footerTop}>
+                    <div className={styles.footerContent}>
+                        <h4>Emiri Sato</h4>
+                        <p>A front-end developer crafting seamless, user-friendly websites and web applications from concept to completion with a focus on great design and usability.</p>
+                    </div>
+                    <div className={styles.footerSocialLinks}>
+                        <h4>Social</h4>
+                        <div className="social-links">
+                            <a href="https://www.linkedin.com/in/emirisato/" target='_blank' className={`${styles.socialIconLink} social-icon-link`}><FaLinkedin className='social-icon'/></a>
+                            <a href="https://github.com/emirisato1003" target='_blank' className={`${styles.socialIconLink} social-icon-link`}><FaGithub className='social-icon'/></a>
+                            <a href="http://www.youtube.com/@e.schannel1997" target='_blank' className={`${styles.socialIconLink} social-icon-link`}><FaYoutube className='social-icon'/></a>
+                        </div>
+                    </div>
                 </div>
-                <div className="footer-social-link">
-                    <a href="https://www.linkedin.com/in/emirisato/" target='_blank' className='social-icon-link'><FaLinkedin /></a>
-                    <a href="https://github.com/emirisato1003" target='_blank' className='social-icon-link'><FaGithub /></a>
-                    <a href="http://www.youtube.com/@e.schannel1997" target='_blank' className='social-icon-link'><FaYoutube /></a>
+                <div className={styles.footerBottom}>
+                    <p>&copy; Copyright 2025. Made by <b>Emiri Sato</b></p>
                 </div>
-            </div>
-            <hr />
-            <div className="footer-bottom">
-                <p>&copy; Copyright 2025. Made by <b>Emiri Sato</b></p>
             </div>
         </footer>
     );
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Home.module.css';
+import projectsData from '../data/projects.json';
 
 //react icon
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 
+// npm library
+import { ScrollMenu } from 'react-horizontal-scrolling-menu';
+
 export default function Home() {
+    const projects = projectsData.projects;
+    const [featuredProjects, setFeaturedProjects] = useState(projects.filter(p => p.featured === true));
+    const featuredProEl = featuredProjects.map(p => (
+        <div key={p.id} className={styles.featuredProjectBox}>
+            <img src={p.pageImage} alt={`${p.title} image`} />
+            <h2>{p.title}</h2>
+            {/* <p>{p.context}</p> */}
+        </div>
+    ));
     return (
-        <main className='home-main gradient'>
-            <h1>hi, my name is emiri sato</h1>
-            <h2>A front-end developer crafting seamless, user-friendly websites and web applications from concept to completion with a focus on great design and usability.</h2>
-            <Link to='/projects'><button>See my projects</button></Link>
-            <div className="social-links">
-                <a href="https://www.linkedin.com/in/emirisato/" target='_blank' className='social-icon-link'><FaLinkedin /></a>
-                <a href="https://github.com/emirisato1003" target='_blank' className='social-icon-link'><FaGithub /></a>
-                <a href="http://www.youtube.com/@e.schannel1997" target='_blank' className='social-icon-link'><FaYoutube /></a>
+        <main className={`${styles.hero} gradient`}>
+            <div className={styles.heroContent}>
+                <h1>hi, my name is emiri sato</h1>
+                <h2>A front-end developer crafting seamless, user-friendly websites and web applications from concept to completion with a focus on great design and usability.</h2>
             </div>
+            <div className='social-links'>
+                <a href="https://www.linkedin.com/in/emirisato/" target='_blank' className='social-icon-link'><FaLinkedin className='social-icon' /></a>
+                <a href="https://github.com/emirisato1003" target='_blank' className='social-icon-link'><FaGithub className='social-icon' /></a>
+                <a href="http://www.youtube.com/@e.schannel1997" target='_blank' className='social-icon-link'><FaYoutube className='social-icon' /></a>
+            </div>
+            <div className={styles.featuredProjects}>
+                <h2>Featured Projects</h2>
+            </div>
+            <Link to='/projects'><button className={styles.ctaBtn}>See all my projects</button></Link>
         </main>
     );
 }
