1b857adb12da198abe4bd2d0c09802f1f7852fce
Emiri Sato <emirisato.1003@gmail.com>
2025-07-12 09:57:38 -0700

    add images for each project

--- src/page/Projects.jsx
+++ src/page/Projects.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import projectsData from '../data/projects.json';
 import { Link } from 'react-router-dom';
+import styles from './Projects.module.css'
 
 export default function Projects() {
     const [projects, setProjects] = useState(projectsData.projects);
     
     const projectsEl = projects.map(project => (
-        <article key={project.id} className='project'>
-            <div className="thumbnail">
+        <article key={project.id} className={styles.projectContainer}>
+            <div className={styles.projectImgContainer}>
                 <img src={project.thumbnail} alt={`${project.title} thumbnail`} />
             </div>
-            <div className="linkContainer">
+            <div className={styles.linkContents}>
                 <h1>{project.title}</h1>
                 <p>{project.context}</p>
                 <Link to={`/projects/${project.id}`}><button>learn more</button></Link>
