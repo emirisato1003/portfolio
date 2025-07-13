4cc6ab704e6825c8525b273a8c2763fd25acf699
Emiri Sato <emirisato.1003@gmail.com>
2025-07-13 14:35:30 -0700

    complete about page styling.

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import certificates from '../data/certificates.json';
+import styles from './About.module.css';
+import { Link } from 'react-router-dom';
 
 export default function About() {
+    const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Python', 'PHP', 'Django', 'Object-Oriented Programming (OOP)', 'Bootstrap', 'SQL', 'JSON', 'Figma', 'Responsive UIs', 'Squarespace', 'Wix', 'Git', 'Github'];
     return (
         <main className='gradient'>
-            <h1 className='heading'>About page</h1>
+            <div className="main-container">
+                <div className={`${styles.aboutIntro} heading`}>
+                    <h1>About me</h1>
+                    <span>As a recent graduate with an Associate’s degree in Web Development, I am fluent in both Japanese and English. Raised in Japan and now based in the US, my diverse background allows me to adapt to different cultural contexts and provide web solutions that connect with users worldwide.</span>
+                </div>
+                <div className={styles.aboutContents}>
+                    <div className={styles.certificates}>
+                        <h2>Certificates</h2>
+                        <ul className={styles.certificatesLists}>
+                            {certificates.certificates.map(cert =>
+                                <li key={cert.id}>
+                                    <a href={cert.credential}>{cert.title}</a>
+                                    <p className={styles.credentialDate}><span><b>{cert.organization}</b></span> - Issued {cert.issuedDate}</p>
+                                </li>)}
+                        </ul>
+                    </div>
+                    <div className={styles.skills}>
+                        <h2>Skills</h2>
+                        <ul className={styles.skillsLists}>
+                            {skills.map(skill => <li key={skill}>{skill}</li>)}
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+//react icon
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+
 export default function Contact() {
     return (
         <main className='gradient'>
-            <h1>Contact page</h1>
+            <h1>Contact</h1>
+            <p>Please reach out if you have any questions! I'm happy to jump on a video call to brainstorm projects and ideas. <br /> Send me an email at <a href="mailto:emirisato1003@gmail.com">emirisato1003@gmail.com</a></p>
+
+            <h1>Social</h1>
+            <div className='social-links'>
+                <a href="https://www.linkedin.com/in/emirisato/" target='_blank' className='social-icon-link'><FaLinkedin className='social-icon' /></a>
+                <a href="https://github.com/emirisato1003" target='_blank' className='social-icon-link'><FaGithub className='social-icon' /></a>
+                <a href="http://www.youtube.com/@e.schannel1997" target='_blank' className='social-icon-link'><FaYoutube className='social-icon' /></a>
+            </div>
         </main>
     );
 }
